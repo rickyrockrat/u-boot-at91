9610d60fb16c265229b984ae65de32e4921afd0d
B.J. Ward IV <kingpuza@gmail.com>
2020-08-13 13:21:30 -0600

    adjusting address space

--- include/configs/at91-sama5_common.h
+++ include/configs/at91-sama5_common.h
@@ -70,8 +70,8 @@
 /* u-boot env in serial flash, by default is bus 0 and cs 0 */
 #define CONFIG_BOOTCOMMAND		"sf probe 0; "				\
 					"sf read 0x21000000 0xB0000 0xd000; "	\
-					"sf read 0x22000000 0xC0000 0x5a0000; "	\
-					"bootm 0x22000000 - 0x21000000"
+					"sf read 0x22400000 0xC0000 0x5a0000; "	\
+					"bootm 0x22400000 - 0x21000000"
 #elif CONFIG_QSPI_BOOT
 #define CONFIG_BOOTCOMMAND		"sf probe 0; "					\
 					"sf read 0x21000000 0x180000 0x80000; "		\
